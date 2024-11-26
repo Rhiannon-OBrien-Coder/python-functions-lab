8899c899f30deb30ba15cb080d4bda592b292429
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-11-25 22:18:36 -0600

    exercise 4 complete

--- exercises.py
+++ exercises.py
@@ -49,10 +49,37 @@
 #
 # Define your function and call it to display the discounted price.
 
-def apply_discount(p, d):
-    percent = d / 100
-    discount = p * percent
-    price = p - discount
-    return(int(price))
+# def apply_discount(p, d):
+#     percent = d / 100
+#     discount = p * percent
+#     price = p - discount
+#     return(int(price))
 
-print('Exercise 3:', apply_discount(100, 25))
+# print('Exercise 3:', apply_discount(100, 25))
+
+
+# Exercise 4: Convert Temperature
+#
+# Write a function called `convert_temperature` that takes a
+# temperature and a unit ('C' for Celsius, 'F' for Fahrenheit)
+# and converts the temperature to the other unit.
+# The formula for converting Celsius to Fahrenheit is (Celsius * 9/5) + 32.
+# The formula for converting Fahrenheit to Celsius is (Fahrenheit - 32) * 5/9.
+#
+# Examples:
+# convert_temperature(0, 'C') should return 32.0.
+# convert_temperature(32, 'F') should return 0.0.
+#
+# Define the function and then call it below.
+
+def convert_temperature(num, temp):
+    if temp == "C":
+        new_temp = (num * 9/5) + 32
+        return(new_temp)
+    elif temp == "F":
+        new_temp = (num - 32) * 5/9
+        return(new_temp)
+
+print('Exercise 4: Convert 0°C to Fahrenheit:', convert_temperature(0, 'C'))
+print('Exercise 4: Convert 32°F to Celsius:', convert_temperature(32, 'F'))
+
